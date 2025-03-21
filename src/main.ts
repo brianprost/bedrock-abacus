a41317d658f08e7d959eb595f7882caef7112249
Brian Prost <me@brianprost.com>
2025-03-21 15:31:34 -0400

    chore: Bump version to 0.2.1 and export tokenize function

--- src/main.ts
+++ src/main.ts
@@ -74,3 +74,5 @@ export function countTokens(text: string): number {
 function findModelNameById(modelId: string): string | undefined {
 	return Object.entries(MODELS).find(([_, id]) => id === modelId)?.[0];
 }
+
+export { tokenize } from "./tokenize.ts";
